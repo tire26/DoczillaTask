f2b35879830a31eb2eda8cecad2a6bd45b2f8ac5
Ivan <73220462+tire26@users.noreply.github.com>
2023-12-24 16:20:02 +0300

    добавлен README.md добавлен ввод пути к корневой директории в качетсве аргумента

---
+++ src/main/README.md
@@ -0,0 +1,3 @@
+- Необходимо собрать проект с помощью mvn package
+- Для запуска программы необходимо выполнить следующую команду: java -jar [DoczillaTask-1.0-SNAPSHOT.jar](..%2F..%2Ftarget%2FDoczillaTask-1.0-SNAPSHOT.jar) 
+  C:\pathToFolder
--- src/main/java/org/ru/Main.java
+++ src/main/java/org/ru/Main.java
@@ -3,7 +3,13 @@ package org.ru;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Usage: java -jar YourJarName.jar <folder_path>");
+            System.exit(1);
+        }
+
+        String folderPath = args[0];
         FileDependencyResolver fileDependencyResolver = new FileDependencyResolver();
-        fileDependencyResolver.startResolving("D:\\Programms\\DoczillaTask\\src\\main\\resources\\test-folder");
+        fileDependencyResolver.startResolving(folderPath);
     }
 }
